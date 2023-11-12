df71a622413a4b0cbe675fc47b182e919aca7214
Marika Shanahan <marikashanahan@gmail.com>
2023-11-11 22:51:40 -0700

    First Pass

--- src/app/page.js
+++ src/app/page.js
@@ -2,27 +2,20 @@
 
 import Question from "@/components/Question"
 import data from "@/data"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function Home () {
-  const [questions, setQuestions] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setQuestions(data)
-      setLoading(false);
-    }, 1500);
-  }, [])
+  const [questions, setQuestions] = useState(data)
 
   return (
-    <div className="w-screen h-full">
-      <h1>Frequently Asked Questions</h1>
-      { questions.length ? 
-        questions.map(({ question, answer}) => <Question key={`ques_${question.substring(0, 10)}`} answer={answer} />) :
-        <p>No questions</p> 
-      }
+    <div className="w-screen h-screen">
+      <h1 className="">Frequently Asked Questions</h1>
+      <div className="grid gap-4">
+        { questions.length ? 
+          questions.map(({ question, answer }, i) => <Question key={`${question.substring(0, 10)}`} question={question} answer={answer} open={i === 0} />) :
+          <p>No questions</p> 
+        }
+      </div>
     </div>
   )
 }
